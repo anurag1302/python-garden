3e5253dbbd58374905cfc5db8c0d9131dd1d04e4
anurag1302 <anurag3487@gmail.com>
2025-10-03 21:24:18 +0530

    init

---
+++ app.py
@@ -0,0 +1,23 @@
+while True:
+
+    print("***********************Welcome to CALCI**************************")
+    print("Please Enter 2 numbers")
+
+    num1 = input("Enter 1st number \n");
+    if num1.lower()=="exit":
+        break
+    num1 = float(num1);
+    num2 = input("Enter 2nd number \n");
+    num2 = float(num2);
+    operator = input("enter the operator -> +,-,*,/");
+
+    if operator=="+":
+        print("Result = ",num1+num2);
+    elif operator=="-":
+        print("Result = ",num1-num2);
+    elif operator=="*":
+        print("Result = ",num1*num2);
+    elif operator=="/":
+        print("Result = ",num1/num2);
+    else:
+        print("Invalid Operator");
