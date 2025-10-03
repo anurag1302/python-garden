605a8c880cbb6365163f742978c4d8238739bd75
anurag1302 <anurag3487@gmail.com>
2025-10-03 21:40:50 +0530

    formatted

--- app.py
+++ app.py
@@ -3,21 +3,21 @@ while True:
     print("***********************WELCOME TO CALCI**************************")
     print("Please Enter 2 numbers")
 
-    num1 = input("Enter 1st number \n");
-    if num1.lower()=="exit":
+    num1 = input("Enter 1st number \n")
+    if num1.lower() == "exit":
         break
-    num1 = float(num1);
-    num2 = input("Enter 2nd number \n");
-    num2 = float(num2);
-    operator = input("enter the operator -> +,-,*,/");
+    num1 = float(num1)
+    num2 = input("Enter 2nd number \n")
+    num2 = float(num2)
+    operator = input("enter the operator -> +,-,*,/")
 
-    if operator=="+":
-        print("Result = ",num1+num2);
-    elif operator=="-":
-        print("Result = ",num1-num2);
-    elif operator=="*":
-        print("Result = ",num1*num2);
-    elif operator=="/":
-        print("Result = ",num1/num2);
+    if operator == "+":
+        print("Result = ", num1 + num2)
+    elif operator == "-":
+        print("Result = ", num1 - num2)
+    elif operator == "*":
+        print("Result = ", num1 * num2)
+    elif operator == "/":
+        print("Result = ", num1 / num2)
     else:
-        print("Invalid Operator");
+        print("Invalid Operator")
